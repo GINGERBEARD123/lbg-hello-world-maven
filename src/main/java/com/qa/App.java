8e95da972787af41d5c142ec01c994a8dc273afe
Lee <purdy990@gmail.com>
2024-12-03 13:52:41 +0000

    EDIT: Updated app.java with 3 new names

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -13,6 +13,9 @@ public class App
         System.out.println(sayHelloToSomeone("Bob"));
         System.out.println(sayHelloToSomeone("Charlie"));
         System.out.println(sayHelloToSomeone("Zena"));
+        System.out.println(sayHelloToSomeone("Lee"));
+        System.out.println(sayHelloToSomeone("Victoria"));
+        System.out.println(sayHelloToSomeone("Adam"));
         System.out.println(sayGoodbye());
     }
 
